ada99ce5b918d980beabc7999045e8ff67744890
unknown <1308830@RG43042.rgu.ac.uk>
2016-02-05 14:28:54 +0000

    Initial commit

---
+++ .idea/AzureTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 1308830
+ * Date: 05/02/2016
+ * Time: 14:20
+ */
+class AzureTest
+{
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>Deployment Test</title>
+</head>
+<body>
+<H1> It is ALIVE!!! (when you see this bit- live) </H1>
+</body>
+</html>
+}
